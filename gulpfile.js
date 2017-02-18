7526e24d8934b8a56b16896deaf99eaa1a38237b
marcelodavid <marcelo.arevalos@hotmail.com>
2017-02-18 14:22:34 -0300

    observar cambio de style.scss con gulp.watch

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,13 @@ const sass = require('gulp-sass');
 
 gulp.task('sass', function(){
     return gulp.src('stylesheets/style.scss')
-        .pipe(sass())
+        .pipe(sass())   // compilar sass
         .pipe(gulp.dest('stylesheets/'));
+});
+
+// observar cambio en style.scss para compilar automaticamente
+gulp.task('watch', function(){
+    gulp.watch('stylesheets/style.scss', ['sass']);    
 });
 
 gulp.task('default', ['sass']);
